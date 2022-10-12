31b3bd9b8e74a53708ff2557cdb7c71fb5bbe8df
BAMobile <78639992+BAMobile@users.noreply.github.com>
2022-10-12 11:44:48 +0300

    Update Program.cs

--- ClassesBasic/Program.cs
+++ ClassesBasic/Program.cs
@@ -1,142 +1,194 @@
-﻿
 using System.Text;
 
 abstract class Worker
 {
-    public string Name { get; }
-    public string Position, WorkDay;
-    public Worker(string name)
-    {
-        Name = name;
-    }
+	public string Name { get; }
+	public string WorkDay { get; private set; }
+	public string Position { get; protected set; }
+	public Worker(string name)
+	{
+		Name = name;
+	}
 
-    void AddTask(string task)
-    {
-        WorkDay = string.IsNullOrEmpty(WorkDay) ? task : WorkDay + $", {task}";
-    }
-    public void Call()
-    {
-        AddTask("Call");
-        //Console.WriteLine("Caaall");
-    }
-    public void WriteCode()
-    {
-        AddTask("WriteCode");
-        //Console.WriteLine("Write Code");
-    }
-    public void Relax()
-    {
-        AddTask("Relax");
-        //Console.WriteLine("Relaaaax");
-    }
-    public abstract void FillWorkDay();
+	void AddTask(string task)
+	{
+		WorkDay = string.IsNullOrEmpty(WorkDay) ? task : $"{WorkDay}, {task}";
+	}
+
+	public void Call()
+	{
+		AddTask("Call");
+	}
+
+	public void WriteCode()
+	{
+		AddTask("WriteCode");
+	}
+
+	public void Relax()
+	{
+		AddTask("Relax");
+	}
+
+	public abstract void FillWorkDay();
 }
 
 class Developer : Worker
 {
-    public Developer(string name) : base(name)
-    {
-        Position = "Розробник";
-    }
-    
-    public override void FillWorkDay()
-    {
-        WriteCode(); Call(); Relax(); WriteCode();
-    }
+	public Developer(string name) : base(name)
+	{
+		Position = "Розробник";
+	}
+
+	public override void FillWorkDay()
+	{
+		WriteCode(); Call(); Relax(); WriteCode();
+	}
 }
 
 class Manager : Worker
 {
-    private Random rand = new Random();
-    public Manager(string name) : base(name)
-    {
-        Position = "Менеджер";
-    }
+	private Random _rand;
+	public Manager(string name) : base(name)
+	{
+		Position = "Менеджер";
+		_rand = new Random();
+	}
 
-    public override void FillWorkDay()
-    {
-        for (int i = 0; i < rand.Next(1, 10); i++)
-        {
-            Call();
-        }
-        Relax();
-        for (int i = 0; i < rand.Next(1, 5); i++)
-        {
-            Call();
-        }
-    }
+	public override void FillWorkDay()
+	{
+		for (int i = 0; i < _rand.Next(1, 10); i++)
+		{
+			Call();
+		}
+		Relax();
+		for (int i = 0; i < _rand.Next(1, 5); i++)
+		{
+			Call();
+		}
+	}
 }
 
 class Team
 {
-    public string Name { get; }
-    private List<Worker> workers = new List<Worker>();
-    public Team(string name)
-    {
-        Name = name;
-    }
-    public void AddWorker(Worker worker)
-    {
-        worker.FillWorkDay();
-        workers.Add(worker);
-    }
-    public void InfoAboutTeam()
-    {
-        Console.WriteLine("Назва команди: {0}", Name);
-        foreach (var worker in workers)
-        {
-            Console.WriteLine(worker.Name);
-        }
-    }
-    public void DetailInfoAboutTeam()
-    {
-        Console.WriteLine("Назва команди: {0}", Name);
-        foreach (var worker in workers)
-        {
-            Console.WriteLine("{0} - {1} - {2}", worker.Name, worker.Position, worker.WorkDay);
-        }
-    }
+	public string Name { get; }
+	// так одразу видно глобальну приватну змінну
+	private List<Worker> _workers;
+
+	public Team(string name)
+	{
+		Name = name;
+		_workers = new List<Worker>();
+	}
+	public void AddWorker(Worker worker)
+	{
+		worker.FillWorkDay();
+		_workers.Add(worker);
+	}
+
+	public void InfoAboutTeam()
+	{
+		baseInfo();
+		foreach (var worker in _workers)
+		{
+			Console.WriteLine(worker.Name);
+		}
+	}
+
+	public void DetailInfoAboutTeam()
+	{
+		baseInfo();
+		foreach (var worker in _workers)
+		{
+			// легше читається текст
+			Console.WriteLine($"{worker.Name} - {worker.Position} - {worker.WorkDay}");
+		}
+	}
+
+	private void baseInfo()
+	{
+		Console.WriteLine($"Назва команди: {Name}");
+		if (_workers.Count == 0)
+		{
+			Console.WriteLine(@"Немає співробітників");
+		}
+	}
 }
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.OutputEncoding = Encoding.UTF8;
-        Console.Write("Введіть назву команди: ");
-        string team_name = Console.ReadLine();
-        Team t = new Team(team_name);
+	static void Main(string[] args)
+	{
+		Console.OutputEncoding = Encoding.UTF8;
+		Console.Write("Введіть назву команди: ");
+		string team_name = Console.ReadLine();
+		Team team = new Team(team_name);
 
-        bool exit = true;
-        do
-        {
-            Console.WriteLine("'eхit' - щоб вийти, 'add' - щоб додати співробітника, 'info' - інфа про команду, 'dinfo' - детальна інфа про команду ");
-            string command = Console.ReadLine();
-            if (command == "exit") exit = false;
-            else if (command == "add")
-            {
-                Console.Write("Введіть ПІП співробітника ");
-                string worker_name = Console.ReadLine();
-                Console.Write("Введіть тип співробітника: 'd' - Розробник 'm' - Менеджер ");
-                string worker_type = Console.ReadLine();
+		bool exit = true;
+		do
+		{
+			// для назв команд бажано було обрати цифри або укр. літери,
+			// бо зараз доводиться весь час перемикатися між англ та укр мовами
 
-                if (worker_type == "d")
-                {
-                    Developer d = new Developer(worker_name);
-                    t.AddWorker(d);
-                }
-                else if (worker_type == "m")
-                {
-                    Manager m = new Manager(worker_name);
-                    t.AddWorker(m);
-                }
-                else Console.WriteLine("Не коректне введення");
-            }
-            else if (command == "info") t.InfoAboutTeam();
-            else if (command == "dinfo") t.DetailInfoAboutTeam();
-            else Console.WriteLine("Не коректне введення");
-        }
-        while (exit);
+			Console.WriteLine(@"'eхit' - щоб вийти, 'add' - щоб додати співробітника, 'info' - інфа про команду,
+								'dinfo' - детальна інфа про команду ");
+			string command = Console.ReadLine();
+			switch (command)
+			{
+				case "exit":
+					{
+						exit = false;
+						break;
+					}
+				case "add":
+					{
+						addWorker(team);
+						break;
+					}
+				case "info":
+					{
+						team.InfoAboutTeam();
+						break;
+					}
+				case "dinfo":
+					{
+						team.DetailInfoAboutTeam();
+						break;
+					}
+				default:
+					{
+						Console.WriteLine(@"Не коректне введення");
+						break;
+					}
+			}
+		}
+		while (exit);
 
-        Console.ReadKey();
-    }
+		Console.ReadKey();
+	}
+
+	private static void addWorker(Team team)
+	{
+		Console.Write(@"Введіть ПІП співробітника ");
+		string worker_name = Console.ReadLine();
+		Console.Write(@"Введіть тип співробітника: 'd' - Розробник 'm' - Менеджер ");
+		string worker_type = Console.ReadLine();
+		switch (worker_type)
+		{
+			case "d":
+				{
+					team.AddWorker(new Developer(worker_name));
+					break;
+				}
+			case "m":
+				{
+					team.AddWorker(new Manager(worker_name));
+					break;
+				}
+			default:
+				{
+					Console.WriteLine(@"Не коректне введення");
+					break;
+				}
+		}
+	}
 }
